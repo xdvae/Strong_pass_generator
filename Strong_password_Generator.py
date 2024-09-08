0ebbf22bf2216d00fe4c01c3439d0f27c04da525
Vaibhav <155850471+xdvae@users.noreply.github.com>
2024-09-08 16:09:02 +0530

    Update Strong_password_Generator.py

--- Strong_password_Generator.py
+++ Strong_password_Generator.py
@@ -5,6 +5,7 @@ import random
 alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = [0,1,2,3,4,5,6,7,8,9] 
 
+# Below is the import Var's required for this.
 alp_size = len(alphabets)
 num_size = len(numbers)
 alp_needed = int(input("How many alphabets do you want: "))
@@ -15,30 +16,29 @@ password = []
 alp_generated = 0
 num_generated = 0
 
+# Generating the pass:
 while len(password) < pass_length:
     rand_num = random.randint(1,2)
 
     if rand_num == 1 and alp_generated < alp_needed:
         rand_num = random.randint(0 , alp_size - 1)
-
         word = alphabets[rand_num]
 
         if rand_num % 2 == 0:
-
             password.append(word.capitalize())
             alp_generated+=1
+            
         else:
             password.append(word)
             alp_generated+=1
 
     
     elif rand_num != 1 and num_generated < num_needed:
-
         rand_num = random.randint(0 , num_size - 1)
         password.append(numbers[rand_num])
         num_generated+=1
 
-
+# Joins the (password) list into a string.
 pass_str = ''.join(map(str,password))
 print(pass_str)
 
